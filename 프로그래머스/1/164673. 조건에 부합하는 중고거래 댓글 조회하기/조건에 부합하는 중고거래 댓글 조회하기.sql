4b24385aca6b89cffd0dea57d9100d749cc6baf1
sunmilk75 <145519399+sunmilk75@users.noreply.github.com>
2024-06-03 22:41:53 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,20 @@
+SELECT 
+    ugb.TITLE, 
+    ugr.BOARD_ID,
+    ugr.REPLY_ID,
+    ugr.WRITER_ID,
+    ugr.CONTENTS,
+    DATE_FORMAT (ugr.CREATED_DATE , '%Y-%m-%d') AS CREATED_DATE 
+FROM 
+    USED_GOODS_BOARD ugb
+INNER JOIN 
+    USED_GOODS_REPLY ugr
+ON 
+    ugb.BOARD_ID = ugr.BOARD_ID
+WHERE 
+    ugb.CREATED_DATE > '2022-09-30'
+AND ugb.CREATED_DATE < '2022-11-01'
+ORDER BY 
+    ugr.CREATED_DATE ASC, 
+    ugb.TITLE ASC
+;
