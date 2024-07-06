490eb92147b3faa9830db60fe715717af5af5939
sunmilk75 <145519399+sunmilk75@users.noreply.github.com>
2024-07-06 15:48:48 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 172 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        System.out.println(str.length());
+    }
+}
