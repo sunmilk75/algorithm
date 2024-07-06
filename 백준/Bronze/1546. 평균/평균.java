e01830f5de7c0b0acd2348c1d97a4051346604fd
sunmilk75 <145519399+sunmilk75@users.noreply.github.com>
2024-07-06 14:05:18 +0900

    [Bronze I] Title: 평균, Time: 212 ms, Memory: 18728 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,23 +6,21 @@ public class Main {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int [] arr = new int[n];
+
+        int max = 0;
         for (int i = 0; i < n; i++) {
-            int x = scn.nextInt();
-            arr[i] = x;
+            arr[i] = scn.nextInt();
+            if(arr[i] > max){
+                max = arr[i];
+            }
         }
 
-        Arrays.sort(arr);
-        int max = arr[arr.length - 1];
-
-        double [] avg = new double[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            double score = ((double)arr[i]/max) * 100;
-            avg[i] = score;
-        }
         double sum = 0;
-        for(double i : avg){
-            sum += i;
+        for (int i = 0; i < n; i++) {
+            sum += (double)arr[i]/max * 100;
         }
-        System.out.println(sum/n);
+
+        double avg = sum / n;
+        System.out.println(avg);
     }
 }
