83acab4753c0c6da18712e87d679111528d786ba
sunmilk75 <145519399+sunmilk75@users.noreply.github.com>
2024-07-14 23:55:23 +0900

    [Bronze II] Title: 문자열 반복, Time: 108 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,31 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class Main {
-    public static void main(String[] args) {
-
-        Scanner scn = new Scanner(System.in);
-
-        int n = scn.nextInt();
-        List<Integer> repetitions = new ArrayList<>();
-        List<String> strings = new ArrayList<>();
-        for(int i = 0; i < n; i++) {
-            int r = scn.nextInt();
-            String str = scn.next();
-            repetitions.add(r);
-            strings.add(str);
-        }
-
-        for (int i = 0; i < n; i++) {
-            int r = repetitions.get(i);
-            String str = strings.get(i);
-            for (int j = 0; j < str.length(); j++) {
-                for (int k = 0; k < r; k++) {
-                    System.out.print(str.charAt(j));
-                }
-            }
-            System.out.println();
-        }
-    }
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < T; i++) {
+	
+			String[] str = br.readLine().split(" ");	// 공백 분리
+			
+			int R = Integer.parseInt(str[0]);	// String -> int
+			String S = str[1];
+			
+			for(int j = 0; j < S.length(); j++) {
+				for(int k = 0; k < R; k++) {
+					System.out.print(S.charAt(j));
+				}
+			}
+			System.out.println();
+		}
+	}
+ 
+ 
 }
