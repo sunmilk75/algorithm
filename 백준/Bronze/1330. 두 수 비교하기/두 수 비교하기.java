89380870fa356f0175eb3e9ff2b2d7472fbee702
sunmilk75 <145519399+sunmilk75@users.noreply.github.com>
2024-07-21 18:46:42 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 128 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,38 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-		Scanner scn = new Scanner(System.in);
-	/*	두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
-		입력
-		첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		출력
-		첫째 줄에 다음 세 가지 중 하나를 출력한다.
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
-		A가 B보다 큰 경우에는 '>'를 출력한다.
-		A가 B보다 작은 경우에는 '<'를 출력한다.
-		A와 B가 같은 경우에는 '=='를 출력한다.
-		*/
-		
-		int a = scn.nextInt();
-		int b = scn.nextInt();
-		
-		while(true) {
-			if(a > b) {
-				System.out.println(">");
-				break;
-			}else if(a < b) {
-				System.out.println("<");
-				break;
-			}else {
-				System.out.println("==");
-				break;
-			}
-		}
-		
-		
-	}
+        System.out.println((A > B) ? ">" : ((A < B) ? "<" : "=="));
+    }
 }
